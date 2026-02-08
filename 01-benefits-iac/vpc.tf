2642a7f5d362f71ed2ca6bfc0ff1793cb4a68547
Gustavo <kkowalskigustavo@gmail.com>
2026-02-08 14:27:06 -0300

    vpc, subnet, internet gateway, route tables

--- 01-benefits-iac/vpc.tf
+++ 01-benefits-iac/vpc.tf
@@ -5,18 +5,10 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "vpc-main"
   }
-  
-  resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "igw-demo"
-  }
-}
 
 resource "aws_subnet" "public-subnet" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.0.0/24" // Diferente em cada subnet para n causar problemas
 
   tags = {
     Name = "public-subnet"
@@ -32,11 +24,19 @@ resource "aws_subnet" "private-subnet" {
   }
 }
 
+ resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw-demo"
+  }
+}
+
 resource "aws_route_table" "route-table-public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
@@ -48,4 +48,9 @@ resource "aws_route_table" "route-table-public" {
   tags = {
     Name = "example"
   }
+}
+
+resource "aws_route_table_association" "public_subnet" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.route-table-public.id
 }
